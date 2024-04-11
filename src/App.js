010f86d5851f0dd99752da2c7883824c0f10943d
Kislay Raj <kislayraj751@gmail.com>
2024-04-11 10:16:12 +0530

    bmi calculator

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import logo from './logo.svg';
-import './bmicalculator.css'
 import './App.css';
 import BmiCalculator from './projects/bmi calculator/BmiCalculator';
+import {Routes, Route} from 'react-router-dom'
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     <BmiCalculator/>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Web Development
-        </a>
-      </header>
->>>>>>> f3fab595b9c378acea8d82f4839afa98d789c76f
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bmicalculator" element={<BmiCalculator />} />
+      </Routes>
+   
     </div>
   );
 }
