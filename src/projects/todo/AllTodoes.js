8d1d2d1a4a185771c9d532753096ef90334adcd9
Kislay Raj <kislayraj751@gmail.com>
2024-04-17 10:03:03 +0530

    17april

--- src/projects/todo/AllTodoes.js
+++ src/projects/todo/AllTodoes.js
@@ -1,26 +1,66 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Dropdown } from "flowbite-react";
 const AllTodoes = () => {
+
+const [todo, setTodo]=useState("");
+const [allTodoes, setAllTodoes]=useState([])
+
+
+// allTodoes=["how are you", "how are you", "how are you", "how"]
+
+const addTodo=()=>
+{
+console.log("kkkkkkkkkk")
+  setAllTodoes([todo,...allTodoes]);
+  setTodo(" ");
+}
+
+
+
   return (
     <div className='bg-blue-300 h-[450px] w-4/5 m-auto rounded-3xl mt-5'>
      
       <div className="flex p-5 gap-10 items-center">
 
+
        <div className=" flex flex-col items-start">
         <div className="">Todo</div>
-        <input type="text" className='w-60 p-2'/>
-       </div>
-       <div className=" flex flex-col items-start ">
-        <div className="">Shedule</div>
-        <input type="password" className='w-60 p-2'/>
+        <input type="text" className='w-60 p-2' value={todo} onChange={(e)=>setTodo(e.target.value)}/>
        </div>
 
+
+
+
+       <div className=" flex flex-col items-start ">
+        <div className="">Shedule</div>
+        <input type="date" className='w-60 p-2'/>
+       </div>
+
+       <div className=" flex flex-col items-start">
+        <div className="">Preority</div>
+        <select type="text" className='w-60 p-3 rounded-md'>
+          <option value="1">Low</option>
+          <option value="2">Medium</option>
+          <option value="3">High</option>         
+        </select>
+       </div>
+  
+
        <div className="bg-blue-700 text-white py-2 px-7 cursor-pointer hover:bg-blue-900 text-xl rounded-lg"
-       onClick={()=>alert("working")}
+       onClick={()=>addTodo()}
        >Add Todo</div>
 
 </div> 
-       
+       {
+       allTodoes?.map((item)=>
+       {
+        return (
+        <div key={item}>
+        {item}
+        </div>
+        )
+       })      
+      }
     </div>
   )
 }
--- src/projects/todo/TodoNavbar.js
+++ src/projects/todo/TodoNavbar.js
@@ -19,18 +19,18 @@ const TodoNavbar = () => {
    <Link to='inprogress'>
    <div className="flex items-center justify-center bg-blue-200 py-2 px-10 rounded-full cursor-pointer min-w-[250px]">
    <div className="">In Progress</div>
-   <div className="bg-blue-500 text-white h-8 w-8 rounded-full ml-3 ">5</div>
+   <div className="bg-blue-500 text-white h-8 w-8 rounded-full ml-3 ">1</div>
    </div></Link>
    <Link to='scheduled'>
    <div className="flex items-center justify-center bg-blue-200 py-2 px-10 rounded-full cursor-pointer min-w-[250px]">
    <div className="">Scheduled </div>
-   <div className="bg-blue-500 text-white h-8 w-8 rounded-full ml-3 ">5</div>
+   <div className="bg-blue-500 text-white h-8 w-8 rounded-full ml-3 ">4</div>
    </div>
    </Link>
    <Link to='completed'>
    <div className="flex items-center bg-blue-200 py-2 px-10  rounded-full cursor-pointer min-w-[250px] text-center">
    <div className="">Completed </div>
-   <div className="bg-blue-500 text-white h-8 w-8 rounded-full ml-3 ">5</div>
+   <div className="bg-blue-500 text-white h-8 w-8 rounded-full ml-3 ">3</div>
    </div>
    </Link>
   </div>
