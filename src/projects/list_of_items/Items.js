1a133b64077a6858b7de78f56af23df4f0aae794
abhinavbgpgit <abhinavbgp@gmail.com>
2024-05-02 03:07:12 +0530

    list of items

---
+++ src/projects/list_of_items/Items.js
@@ -0,0 +1,9 @@
+const items = [
+    { id: 1, title: "Believe you can and you're halfway" },
+    { id: 2, title: "The only way to do great work is to" },
+    { id: 3, title: "Success is not final, failure is not" },
+    { id: 4, title: "Your limitation—it's only your" },
+    { id: 5, title: "Push yourself, because no one else" },
+    { id: 6, title: "Great things never come from comfort" }
+  ];
+export default items;  
--- src/projects/list_of_items/ListOfItems.js
+++ src/projects/list_of_items/ListOfItems.js
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import items from "./Items";
+import { Link } from "react-router-dom";
 const ListOfItems = () => {
+    const [selected, setSelected]=useState(0);
+    const dosomething=(id)=>{
+        setSelected(id);
+        
+    }
   return (
-    <div>ListOfItems</div>
-  )
-}
+    <div className="bg-red-300 w-full h-full center">
+      <div className="w-2/5 h-full bg-blue-200 flex flex-col pt-20 px-5 gap-10  items-start justify-start mt-32 overflow-auto   ">
+        {items.map((list) => (
+          <div
 
-export default ListOfItems
+         
+            className={`w-full border-2 p-5  text-left rounded-lg   ${selected === list.id ?'bg-red-800 text-white':'bg-white hover:bg-red-300'} `}
+            key={list.id}
+          >
+            <div className={`text-lg w-full cursor-pointer`}
+            onClick={()=>dosomething(list.id)}
+            >
+              {list.title}
+            </div>           
+          </div>
+        ))}
+      </div>
+      <div className="w-3/5 h-full center bg-green-200 text-lg ">More Functionality in <Link className="text-blue-600 ml-2" > Here</Link></div>
+   
+    
+    </div>
+  );
+};
+
+export default ListOfItems;
